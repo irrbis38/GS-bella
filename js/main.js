86e4b616afe999ddf02a7120b4d6afb3a5ed32dc
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-02-07 16:27:05 +0300

    feat: add animation to navbar and logo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
-    
-    // start here
+function initNavigation() {
+    const mainNavLinks = gsap.utils.toArray(".main-nav a");
+    const mainNavLinksRev = mainNavLinks.reverse();
 
+    mainNavLinks.forEach((link) => {
+        link.addEventListener("mouseleave", () => {
+            // add class
+            link.classList.add("animate-out");
+            // remove class
+            setTimeout(() => {
+                link.classList.remove("animate-out");
+            }, 300);
+        });
+    });
+
+    function navAnimation({ direction }) {
+        console.log(direction);
+        const isScrollingDown = direction === 1;
+        const links = isScrollingDown ? mainNavLinks : mainNavLinksRev;
+        return gsap.to(links, {
+            duration: 0.3,
+            stagger: 0.05,
+            autoAlpha: () => (isScrollingDown ? 0 : 1),
+            y: () => (isScrollingDown ? 20 : 0),
+            ease: "Power4.out",
+        });
+    }
+
+    ScrollTrigger.create({
+        start: 100,
+        end: "bottom bottom-=20",
+        toggleClass: {
+            targets: "body",
+            className: "has-scrolled",
+        },
+        onEnter: (scrollObject) => navAnimation(scrollObject),
+        onLeaveBack: (scrollObject) => navAnimation(scrollObject),
+        markers: true,
+    });
 }
 
-window.addEventListener('load', function(){
+function init() {
+    initNavigation();
+}
+
+window.addEventListener("load", function () {
     init();
 });
