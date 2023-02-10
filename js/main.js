13ef79d8269c5ab76a3446467b229a2d2d46fe33
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-02-10 16:34:29 +0300

    feat: hide every image in reveal gallery

--- js/main.js
+++ js/main.js
@@ -93,9 +93,33 @@ function moveImages(e) {
     });
 }
 
+function initHoverReveal() {
+    const sections = document.querySelectorAll(".rg__column");
+
+    sections.forEach((section) => {
+        console.log("hello");
+        // get components for animation
+        const imageBlock = section.querySelector(".rg__image");
+        const mask = section.querySelector(".rg__image--mask");
+
+        //reset the initial position
+        gsap.set(imageBlock, { yPercent: -101 });
+        gsap.set(mask, { yPercent: 100 });
+
+        // add event listeners to each section
+        section.addEventListener("mouseenter", createHoverReveal);
+        section.addEventListener("mouseleave", createHoverReveal);
+    });
+}
+
+function createHoverReveal(e) {
+    console.log(e.type);
+}
+
 function init() {
     initNavigation();
     initHeaderTilt();
+    initHoverReveal();
 }
 
 window.addEventListener("load", function () {
