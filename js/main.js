27efc9f9956c744a96f482219a3d20e09bbe3ad3
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-02-13 17:14:39 +0300

    feat: parallax completed

--- js/main.js
+++ js/main.js
@@ -271,11 +271,73 @@ function getPortfolioOffset(clientY) {
     );
 }
 
+function initImageParallax() {
+    // select all section .with-parallax
+    gsap.utils.toArray(".with-parallax").forEach((section) => {
+        // get the image
+        const image = section.querySelector("img");
+
+        // create tween for the image
+        gsap.to(image, {
+            yPercent: 20,
+            ease: "none",
+            scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                scrub: true,
+            },
+        });
+    });
+}
+
+function initPinSteps() {
+    ScrollTrigger.create({
+        trigger: ".fixed-nav",
+        start: "top center",
+        endTrigger: "#stage4",
+        end: "center center",
+        pin: true,
+        // markers: true,
+    });
+
+    const getVh = () => {
+        const vh = Math.max(
+            document.documentElement.clientHeight || 0,
+            window.innerHeight || 0
+        );
+        return vh;
+    };
+
+    const updateBodyColor = (color) => {
+        // gsap.to(".fill-background", { backgroundColor: color, ease: "none" });
+        document.documentElement.style.setProperty("--bgc-fill-color", color);
+    };
+
+    gsap.utils.toArray(".stage").forEach((stage, index) => {
+        const navLinks = gsap.utils.toArray(".fixed-nav li");
+
+        ScrollTrigger.create({
+            trigger: stage,
+            start: "top center",
+            end: () => `+=${stage.clientHeight + getVh() / 10}`,
+            toggleClass: {
+                targets: navLinks[index],
+                className: "is-active",
+            },
+            // markers: true,
+            onEnter: () => updateBodyColor(stage.dataset.color),
+            onEnterBack: () => updateBodyColor(stage.dataset.color),
+        });
+    });
+}
+
 function init() {
     initNavigation();
     initHeaderTilt();
     handleWidthChange(mq);
     initPortfolioHover();
+    initImageParallax();
+    initPinSteps();
 }
 
 window.addEventListener("load", function () {
